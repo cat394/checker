b428bc46043a001a60b809c9e3ad03875bf5b92e
cat394 <keyring1231@gmail.com>
2024-08-26 16:01:19 +0900

    refactor: change type name clearly

--- packages/object/src/checkInstance/main.ts
+++ packages/object/src/checkInstance/main.ts
@@ -1,4 +1,4 @@
-import type { InstancedClass } from "../types.ts";
+import type { Constructor } from "../types.ts";
 
 /**
  * Checks if a given object is an instance of a specified constructor.
@@ -6,7 +6,7 @@ import type { InstancedClass } from "../types.ts";
  * The function determines whether `obj` is an instance of the class or constructor function `constructor`.
  *
  * @param {unknown} value - The object to check.
- * @param {InstancedClass} constructor - The constructor function or class to check against.
+ * @param {Constructor} constructor - The constructor function or class to check against.
  * @returns {boolean} True if the object is an instance of the constructor, otherwise false.
  *
  * @example
@@ -17,11 +17,11 @@ import type { InstancedClass } from "../types.ts";
  * // Returns false
  * checkIsInstance({}, Date); // The object is not an instance of Date
  */
-export const checkIsInstance = <T extends InstancedClass>(
-  value: unknown,
-  constructor: T,
-): value is InstanceType<InstancedClass> => {
-  return value instanceof constructor;
+export const checkIsInstance = <T extends Constructor>(
+	value: unknown,
+	constructor: T
+): value is InstanceType<Constructor> => {
+	return value instanceof constructor;
 };
 
 /**
@@ -30,7 +30,7 @@ export const checkIsInstance = <T extends InstancedClass>(
  * This is the inverse of `checkIsInstance`.
  *
  * @param {unknown} value - The object to check.
- * @param {InstancedClass} constructor - The constructor function or class to check against.
+ * @param {Constructor} constructor - The constructor function or class to check against.
  * @returns {boolean} True if the object is not an instance of the constructor, otherwise false.
  *
  * @example
@@ -41,12 +41,9 @@ export const checkIsInstance = <T extends InstancedClass>(
  * // Returns false
  * checkIsNotInstance(new Date(), Date); // The object is an instance of Date
  */
-export const checkIsNotInstance = <
-  T1 extends unknown,
-  T2 extends InstancedClass,
->(
-  value: T1,
-  constructor: T2,
+export const checkIsNotInstance = <T1 extends unknown, T2 extends Constructor>(
+	value: T1,
+	constructor: T2
 ): value is Exclude<T1, T2> => {
-  return !checkIsInstance(value, constructor);
+	return !checkIsInstance(value, constructor);
 };
